0d35552d04648c32574d25d268d6eb8ba24fa110
Kultinov Ilya <kultinov.i@gmail.com>
2022-06-14 22:17:05 +0300

    v.0.2

--- StackoverflowTags.py
+++ StackoverflowTags.py
@@ -1,12 +1,14 @@
 import requests
 
 
-def getting_questions_on_tag():
-    url = 'https://api.stackexchange.com/2.3/questions?fromdate=1654905600&todate=1655164800&order=desc&sort=activity&tagged=%22Python%22&site=stackoverflow'
-    response = requests.get(url)
+def getting_questions_on_tag(tag):
+    url = 'https://api.stackexchange.com/2.3/questions'
+    params = {'fromdate': '1654905600', 'todate': '1655164800',
+              'tagged': tag, 'site': 'stackoverflow'}
+    response = requests.get(url, params=params)
     for i in response.json()['items']:
         print(i.get('title'))
 
 
 if __name__ == '__main__':
-    getting_questions_on_tag()
+    getting_questions_on_tag("Python")
